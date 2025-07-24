a7596619bf9b7400c04e52fc7acaed80291b1b63
gitraya <mail.raya15@gmail.com>
2025-07-25 01:18:28 +0700

    feat: implement Trip API with serializers and viewsets

--- backend/planner/views.py
+++ backend/planner/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Trip
+from .serializers import TripSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+    permission_classes = [IsAuthenticated]
