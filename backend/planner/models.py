0f81f647747d73a652881ab67c99396daf0d0971
gitraya <mail.raya15@gmail.com>
2025-07-28 23:25:14 +0700

    feat: update user model and migrations; set custom user model and adjust Trip model relationships

--- backend/planner/models.py
+++ backend/planner/models.py
@@ -1,10 +1,16 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class User(AbstractUser):
+    pass
+
+
 class Trip(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='trips')
+    owner = models.ForeignKey("User", on_delete=models.CASCADE, related_name='trips')
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    create_date = models.DateField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
     start_date = models.DateField()
     end_date = models.DateField()
 
