f583f94148d114e7b5eb91c47330a0fd34827133
gitraya <mail.raya15@gmail.com>
2025-11-24 22:41:38 +0700

    feat: update index.html to use jelajah.svg and change title; remove vite.svg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,26 +2,28 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    host: true,
-    port: 5173,
-  },
-  preview: {
-    host: true,
-    port: 4173,
-    allowedHosts: ["jelajah-frontend.onrender.com", "localhost", "127.0.0.1"],
-  },
+    server: {
+      host: true,
+      preview: {
+        host: true,
+        port: 4173,
+        allowedHosts: env.ALLOWED_HOSTS ? env.ALLOWED_HOSTS.split(",") : [],
+      },
+    },
+  };
 });
