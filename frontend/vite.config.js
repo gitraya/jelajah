ac1edf93b4377272d32c876196b6644fd375bc20
gitraya <mail.raya15@gmail.com>
2025-11-24 22:54:20 +0700

    refactor: simplify Vite config export structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,26 +8,25 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    server: {
-      host: true,
-      preview: {
-        host: true,
-        port: 4173,
-        allowedHosts: [
-          "localhost",
-          "127.0.0.1",
-          "jelajah-frontend.onrender.com",
-          "jelajah.raya.bio",
-        ],
-      },
-    },
-  };
+  },
+  server: {
+    host: true,
+    port: 5173,
+  },
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "jelajah-frontend.onrender.com",
+      "jelajah.raya.bio",
+    ],
+  },
 });
